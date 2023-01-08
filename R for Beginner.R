72b8dbe9711b2abe1fcae31af9d0687a48dce044
Ilham Darussalam <97231250+ilhamster19@users.noreply.github.com>
2023-01-08 15:02:53 +0700

    Add files via upload

---
+++ R for Beginner.R
@@ -0,0 +1,62 @@
+
+# R for beginner
+
+#output text
+print("Hello World!!")
+cat("Hello World 1\nHello World 2")
+
+#calculate
+5
+5+5
+10*2
+10/2
+5^2
+sqrt(100)
+
+#variable :
+#numeric
+a<-10
+print(a)
+#or
+a
+b<-20
+b
+c<-a+b
+c
+
+
+#string
+say<-"Hello"
+say
+hi<-"World"
+hi
+cat(say,hi)
+
+
+#logical
+T<-word=="Hello"
+T
+F<-word=="Holla"
+F
+
+#vector
+vector1<-c(1,2,3,4,5,6)
+vector1
+vector2<-c(6,5,4,3,2,1)
+vector2
+
+#matrix
+M<-matrix(c(vector1,vector2),6,2)
+M
+
+#dataframe
+df<-data.frame(vector1,vector2)
+df
+
+#list
+lst<-list(vector1,vector2,M,df)
+lst
+
+#delete variable
+rm(...)
+
